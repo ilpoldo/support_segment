6ed6aafda6a888a9b7f0b92529b5cbb306ef382b
Leandro Pedroni <ilpoldo@gmail.com>
2015-01-28 12:18:29 +0000

    autoload validators

--- lib/support_segment/railtie.rb
+++ lib/support_segment/railtie.rb
@@ -1,11 +1,16 @@
 require 'support_segment/mobile_detect'
 require 'mime/type'
 
+
 module SupportSegment
   class Railtie < Rails::Railtie
     initializer "support_segment.configure_rails_initialization" do |app|
       Mime::Type.register_alias "text/html", :mobile
       app.middleware.use MobileDetect::Middleware
+    end
+
+    initializer 'support_segment.validators', :before => :set_autoload_paths do |app|
+      app.config.autoload_paths << File.expand_path("../../validators", __FILE__)
     end
   end
 end
--- lib/support_segment/sti_helpers.rb
+++ lib/support_segment/sti_helpers.rb
@@ -9,49 +9,52 @@
 # 
 #  Thanks to Alex and coderrr!
 #-------------------------------------------------------------------------------
-module SupportSegment::StiHelpers
-  def self.included(base)
-    base.send :include, InstanceMethods
-    base.extend ClassMethods
-  end
+module SupportSegment
+  module StiHelpers
 
-  module ClassMethods
-
-    def select_options
-      descendants.map{ |c| c.to_s }.sort
+    def self.included(base)
+      base.send :include, InstanceMethods
+      base.extend ClassMethods
     end
 
-    def inherited(child)
-      child.instance_eval do
-        def model_name
-          sti_base_class.model_name
+    module ClassMethods
+
+      def select_options
+        descendants.map{ |c| c.to_s }.sort
+      end
+
+      def inherited(child)
+        child.instance_eval do
+          def model_name
+            sti_base_class.model_name
+          end
         end
+        super
       end
-      super
+
+      def sti_base_class
+        if "#{self.superclass.name}" == "ActiveRecord::Base"
+          return self
+        end
+        return superclass.sti_base_class
+      end
+
+      # !! with the first conditional clause type logic will only apply to base class
+      #     this MAY not be what you'd want, in which case ommit.
+      def new(*a, &b)
+        if (self == sti_base_class) and (h = a.first).is_a? Hash and (type = h[:type] || h['type']) and (klass = type.constantize) != self
+          raise "wtF hax!!"  unless klass < self  # klass should be a descendant of us
+          return klass.new(*a, &b)
+        end
+        super(*a, &b)
+      end
     end
 
-    def sti_base_class
-      if "#{self.superclass.name}" == "ActiveRecord::Base"
-        return self
+    module InstanceMethods
+      def sti_base_class
+        self.class.sti_base_class
       end
-      return superclass.sti_base_class
     end
 
-    # !! with the first conditional clause type logic will only apply to base class
-    #     this MAY not be what you'd want, in which case ommit.
-    def new(*a, &b)
-      if (self == sti_base_class) and (h = a.first).is_a? Hash and (type = h[:type] || h['type']) and (klass = type.constantize) != self
-        raise "wtF hax!!"  unless klass < self  # klass should be a descendant of us
-        return klass.new(*a, &b)
-      end
-      super(*a, &b)
-    end
   end
-
-  module InstanceMethods
-    def sti_base_class
-      self.class.sti_base_class
-    end
-  end
-
 end
